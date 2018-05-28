aa2a85ee713bed08306292fccd45e0ac031d33c7
Cory Logan <he@corylogan.com>
2018-05-28 14:25:42 -0700

    testing

---
+++ src/components/customers.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="customer-list">
+    <h3>Customers</h3>
+    <ul v-for='customer in customers'>
+      <li>{{customer.name}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+ export default {
+   name: 'customer-list',
+   data() {
+     return {
+     }
+   }
+ }
+</script>
---
+++ src/components/firebase-config.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: "AIzaSyCSTp7E0h3eQSvM7jWCwClg5MWAXmAU9aQ",
+  authDomain: "chickadee-41af5.firebaseapp.com",
+  databaseURL: "https://chickadee-41af5.firebaseio.com",
+  projectId: "chickadee-41af5",
+  storageBucket: "chickadee-41af5.appspot.com",
+  messagingSenderId: "142972629636"
+}
