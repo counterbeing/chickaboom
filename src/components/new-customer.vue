3e283963f6adbb8ddabe84eea0df4d6a707a4275
Cory Logan <he@corylogan.com>
2018-05-29 13:35:48 -0700

    save new customers

--- src/components/new-customer.vue
+++ src/components/new-customer.vue
@@ -8,7 +8,7 @@
           <form class="needs-validation" novalidate>
             <div class="mb-3">
               <label for="name">Name</label>
-              <input v-model='name' type="text" class="form-control" id="name" placeholder="Jesus H. Christ" value="" required>
+              <input v-model='customer.name' type="text" class="form-control" id="name" placeholder="Jesus H. Christ" value="" required>
               <div class="invalid-feedback">
                 Valid first name is required.
               </div>
@@ -16,22 +16,22 @@
 
             <div class="mb-3">
               <label for="email">Email</label>
-              <input v-model='email' type="email" class="form-control" id="email" placeholder="you@example.com">
+              <input v-model='customer.email' type="email" class="form-control" id="email" placeholder="you@example.com">
               <div class="invalid-feedback">
                 Please enter a valid email address for  shipping updates.
               </div>
             </div>
 
             <div class="mb-3">
-              <label for="phone">Phone</label>
-              <input v-model='phone' type="tel" class="form-control" id="phone" placeholder="555-555-5555">
+              <label for="customer.phone">Phone</label>
+              <input v-model='customer.phone' type="tel" class="form-control" id="phone" placeholder="555-555-5555">
               <div class="invalid-feedback">
                 Please enter a valid email address for  shipping updates.
               </div>
             </div>
 
             <div class="mb-3"  v-show='!showAddress'>
-              <label for="email">Billing Address</label>
+              <label for="customer.email">Billing Address</label>
               <vue-google-autocomplete
               ref="address"
               id="place-search"
@@ -45,7 +45,7 @@
             <div v-show='showAddress'>
               <div class="mb-3">
                 <label for="address">Address</label>
-                <input v-model='address.address_1' type="text" class="form-control" id="address_1" placeholder="1234 Main St" required>
+                <input v-model='customer.address.address_1' type="text" class="form-control" id="address_1" placeholder="1234 Main St" required>
                 <div class="invalid-feedback">
                   Please enter a billing address.
                 </div>
@@ -54,28 +54,28 @@
               <div class="row">
                 <div class="col-md-4 mb-3">
                   <label for="zip">City</label>
-                  <input v-model='address.city' type="text" class="form-control" id="city" placeholder="" required>
+                  <input v-model='customer.address.city' type="text" class="form-control" id="city" placeholder="" required>
                   <div class="invalid-feedback">
                     Zip code required.
                   </div>
                 </div>
                 <div class="col-md-4 mb-1">
                   <label for="state">State</label>
-                  <input v-model='address.state' type="text" class="form-control" id="state" placeholder="" required>
+                  <input v-model='customer.address.state' type="text" class="form-control" id="state" placeholder="" required>
                   <div class="invalid-feedback">
                     Please provide a valid state.
                   </div>
                 </div>
                 <div class="col-md-4 mb-3">
                   <label for="zip">Zip</label>
-                  <input v-model='address.zip' type="text" class="form-control" id="zip" placeholder="" required>
+                  <input v-model='customer.address.zip' type="text" class="form-control" id="zip" placeholder="" required>
                   <div class="invalid-feedback">
                     Zip code required.
                   </div>
                 </div>
               </div>
             </div>
-            <button class="btn btn-primary btn-lg btn-block" type="submit">Save Customer</button>
+            <button class="btn btn-primary btn-lg btn-block" @click='submit'>Save Customer</button>
       </form>
     </div>
   </div>
@@ -83,38 +83,44 @@
 
 <script>
   import VueGoogleAutocomplete from 'vue-google-autocomplete'
+  import db from './firebase-init'
 
   export default {
     components: { VueGoogleAutocomplete },
     name: 'new-customer',
     data() {
       return {
-        phone: null,
-        name: null,
-        email: null,
-        address: {
-          address_1: null,
-          state: null,
-          zip: null,
-          city: null,
+        customer: {
+          phone: null,
+          name: null,
+          email: null,
+          address: {
+            address_1: null,
+            state: null,
+            zip: null,
+            city: null,
+          },
         },
       }
     },
     methods: {
       getAddressData: function (addressData) {
-        this.address.state = addressData.administrative_area_level_1
-        this.address.zip = addressData.postal_code
-        this.address.city = addressData.locality
-        this.address.country = addressData.country
-        this.address.address_1 = `${addressData.street_number} ${addressData.route}`
-        this.address.address_2 = addressData.city
-        this.address.latitude = addressData.latitude
-        this.address.longitude = addressData.longitude
+        this.customer.address.state = addressData.administrative_area_level_1
+        this.customer.address.zip = addressData.postal_code
+        this.customer.address.city = addressData.locality
+        this.customer.address.country = addressData.country
+        this.customer.address.address_1 = `${addressData.street_number} ${addressData.route}`
+        // this.customer.address.address_2 = addressData.city
+        this.customer.address.latitude = addressData.latitude
+        this.customer.address.longitude = addressData.longitude
+      },
+      submit: function () {
+        db.collection('customers').add(this.customer)
       }
     },
     computed: {
       showAddress() {
-        const a = this.address.address_1
+        const a = this.customer.address.address_1
         return a !== null && a !== ''
       }
     }
