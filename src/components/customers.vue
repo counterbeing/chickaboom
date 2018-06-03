337ad97cedf62ae3c117d0b44072f600f81b81b6
Cory Logan <he@corylogan.com>
2018-06-03 16:22:32 -0700

    style change

--- src/components/customers.vue
+++ src/components/customers.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="customer-list">
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom">
-      <h1 class="h2">Customers</h1>
+      <h1 class="h3">Customers</h1>
       <div class="btn-toolbar mb-2 mb-md-0">
         <div class="btn-group mr-2">
           <router-link class="btn btn-sm btn-outline-secondary" :to="{name: 'new-customer'}">New Customer</router-link>
