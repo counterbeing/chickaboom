1d3a21a131383029eb4f073d3c5e0de8c9152eee
Cory Logan <he@corylogan.com>
2018-08-01 16:48:27 -0700

    add a staging config env

--- src/config/firebase-config.js
+++ src/config/firebase-config.js
@@ -1,4 +1,5 @@
-export default {
+
+const staging = {
   apiKey: "AIzaSyCSTp7E0h3eQSvM7jWCwClg5MWAXmAU9aQ",
   authDomain: "chickadee-41af5.firebaseapp.com",
   databaseURL: "https://chickadee-41af5.firebaseio.com",
@@ -6,3 +7,22 @@ export default {
   storageBucket: "chickadee-41af5.appspot.com",
   messagingSenderId: "142972629636"
 }
+
+const production = {
+  apiKey: "AIzaSyBMpg_j3H5GteZOrsUrMKo-0ozlI6oZuSg",
+  authDomain: "chickaboom-production.firebaseapp.com",
+  databaseURL: "https://chickaboom-production.firebaseio.com",
+  projectId: "chickaboom-production",
+  storageBucket: "chickaboom-production.appspot.com",
+  messagingSenderId: "504527290634"
+}
+
+function hostIsProd() {
+  return window.location.href.indexOf("app") > -1
+}
+
+function chooseDatabase() {
+  return hostIsProd() ? production : staging
+}
+
+export default chooseDatabase()
