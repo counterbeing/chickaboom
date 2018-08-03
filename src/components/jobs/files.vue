bcc7d77c64fe960134b6f4e6b2b4282809225785
Cory Logan <he@corylogan.com>
2018-08-03 16:31:11 -0700

    basic uploading works

--- src/components/jobs/files.vue
+++ src/components/jobs/files.vue
@@ -6,7 +6,7 @@
 
     <div class="col-md-6">
       <h3>Delivered Files</h3>
-      <upload-zone/>
+      <upload-zone :parentFolder="jobId"/>
     </div>
   </div>
 </template>
@@ -14,15 +14,14 @@
 
 <script>
   import UploadZone from '@/components/partials/upload-zone'
-  import Videos from '@/components/partials/videos'
-  import Photos from '@/components/partials/photos'
   import { mapGetters } from 'vuex'
 
    export default {
-     components: { Videos, Photos, UploadZone },
+     components: { UploadZone },
      computed: {
        ...mapGetters(['jobById', 'customers']),
        job() { return this.jobById(this.$route.params.id) },
+       jobId() { return this.$route.params.id }
 
      },
      methods: { }
--- src/components/partials/upload-zone.vue
+++ src/components/partials/upload-zone.vue
@@ -1,16 +1,21 @@
 <template>
   <div class="upload-zone" ref='dropzone'>
-    <p>upload files here</p>
   </div>
 </template>
 
 <script>
+  import firebase from 'firebase/app'
+  import 'firebase/storage'
+
   export default {
     data() {
       return {
-        files: []
+        files: [],
+        uploadTask: null,
+        storageRef: null,
       }
     },
+    props: [ 'parentFolder' ],
     methods: {
       highlight() {
         this.$refs.dropzone.classList.add('highlighted')
@@ -19,9 +24,16 @@
         this.$refs.dropzone.classList.remove('highlighted')
       },
       upload(file) {
-        console.log('trying to upload ' + file)
+        console.log('trying to upload ')
+        console.log(file);
+        const path = this.parentFolder.split('/')
+        path.push(file.name)
+        const fullPath = path.join('/')
+        this.uploadTask = this.storageRef.child(fullPath).put(file);
       },
       handleDrop(e) {
+        console.log('DROPPPED');
+        // console.log(e);
         let dt = e.dataTransfer
         this.files = dt.files
         this.handleFiles()
@@ -31,25 +43,41 @@
       },
     },
     mounted() {
-      console.log('hello i mounted')
       const dropzone = this.$refs.dropzone
+      this.storageRef = firebase.storage().ref();
+
+      function preventDefaults (e) {
+        e.preventDefault()
+        e.stopPropagation()
+      }
+
+      ['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
+        dropzone.addEventListener(eventName, preventDefaults, false)
+      })
+
       ;['dragenter', 'dragover'].forEach(eventName => {
         dropzone.addEventListener(eventName, this.highlight, false)
       })
       ;['dragleave', 'drop'].forEach(eventName => {
         dropzone.addEventListener(eventName, this.unhighlight, false)
       })
-      dropzone.addEventListener('drop', this.handleDrop, false)
+
+      ;['dragdrop', 'drop'].forEach(eventName => {
+        dropzone.addEventListener(eventName, this.handleDrop, false)
+      })
     }
   }
 </script>
 
 <style>
   .upload-zone {
-    background-color: green;
+    border: 2px dashed #ccc;
+    font-family: sans-serif;
+    margin: 100px auto;
+    padding: 20px;
   }
 
   .highlighted {
-    background-color: red;
+    background-color: green;
   }
 </style>
